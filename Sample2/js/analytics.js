6155010ac34c01fc45723389153d34d9c499095d
すー <s1732087ap@s.chibakoudai.jp>
2018-03-20 02:40:43 +0900

    fix + slider

---
+++ Sample2/js/analytics.js
@@ -0,0 +1,6 @@
+document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=UA-123456789-1"></script>');
+window.dataLayer = window.dataLayer || [];
+function gtag(){
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());gtag('config', 'UA-115862516-2');
---
+++ test/1/js/slick.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('.single-item').slick({
+      accessibility: true,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      dots: true,
+      fade: true,
+      arrows: false,        //矢印の表示
+    infinite: true,      //無限ループ
+    centerMode: true
+    });
+  });
