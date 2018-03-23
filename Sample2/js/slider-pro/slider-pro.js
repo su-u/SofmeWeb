0892348fdb6acf94fa0d81c931618d55519b9bcc
すー <s1732087ap@s.chibakoudai.jp>
2018-03-23 18:43:07 +0900

    fix

--- Sample2/js/slider-pro/slider-pro.js
+++ Sample2/js/slider-pro/slider-pro.js
@@ -1,12 +1,13 @@
 $( document ).ready(function( $ ) {
     $( '#slide' ).sliderPro({
       width: 1280,
-      height: 500,
+      height: 600,
       arrows: true,
       buttons: true,
       waitForLayers: true,
       autoplay: true,
       autoplayDelay: 5000,
       autoScaleLayers: true,
+      responsive: true,
     });
-  });
+  }); 
