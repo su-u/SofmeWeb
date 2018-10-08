3b6211e602b3cbc9864c76c8d6a322961c1d1550
すー <aaaiiiuuu35@gmail.com>
2018-10-09 01:12:07 +0900

    整理

--- backUp/18-03-22/Sample2/js/analytics.js
+++
@@ -1,6 +0,0 @@
-document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=UA-123456789-1"></script>');
-window.dataLayer = window.dataLayer || [];
-function gtag(){
-  dataLayer.push(arguments);
-}
-gtag('js', new Date());gtag('config', 'UA-115862516-2');
